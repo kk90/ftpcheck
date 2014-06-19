fc3a245dabe245e15a1187ff41cdc609d6527167
kk90 <kr.korycki@gmail.com>
2014-06-19 16:29:50 +0200

    added kickstartjs

--- server.js
+++ server.js
@@ -1,33 +1,23 @@
 ﻿
 var express = require( 'express' );
-
 var app = express();
-
+app.set('view engine', 'ejs');
+app.use(express.static("views/kickstartJS"));
 var Client = require( 'ftp' );
 var path="/pub/people/L.Jackowska/Lingwistyka";
 // Configuration 
 
 
+
 // Routes 
 app.get( '/', function ( req, res ) {
-
+    
     var c = new Client();
     c.on( 'ready', function () {
         c.list(path, function ( err, list ) {
             if ( err ) throw err;
            
-           list.forEach(function(element){
-             if(element.name==="Test results (studia_dzienne)"){
-                if(element.date.getDay()==5){
-                    res.send("BRAK wyników");
-                    }else{
-                        res.send("WYNIKI")
-                        }
-
-                  }
-               
-               });
-           
+           res.render('layout', { list: list,title:"Lista folderów" });
             c.end();
         });
     });
